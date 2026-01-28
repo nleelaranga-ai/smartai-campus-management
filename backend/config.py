bb80536728a83548f0aaa3c8b46d80810d8cfeb1
nleelaranga-ai <n.leelaranga@gmail.com>
2026-01-28 23:54:30 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,11 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///local.db")
+    # Get URL and fix prefix if necessary
+    uri = os.getenv("DATABASE_URL")
+    if uri and uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+
+    SQLALCHEMY_DATABASE_URI = uri or "sqlite:///local.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "dev-secret")
